f3c4a49e17e057bca083c3bd16e7839127d4e819
Selvin11 <528370630@qq.com>
2016-06-27 20:49:43 +0800

    add

--- js/gsw.js
+++ js/gsw.js
@@ -41,9 +41,55 @@ for (var i = 0; i < checkbox.length; i++) {
 /**
  * add click and add ul
  */
+// click add btn and show the edit area
 var add = document.getElementById('add');
+var editArea = document.getElementsByClassName('edit-area')[0];
 add.onclick = function () {
+  if (editArea.style.display === 'none') {
+    editArea.style.display = 'block';
+    add.style.transform = 'rotate3d(0, 0, 1, -45deg)';
+  }else{
+    editArea.style.display = 'none';
+    add.style.transform = '';
+  }
+}
+
+var middle = document.getElementById('middle');
+// get time
+var weektime = document.getElementById('weektime');
+var now = new Date();
+var weekday = ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'];
+weektime.innerHTML = '今天 ' + weekday[now.getDay()];
+// set date
+var date = document.getElementById('date');
+date.value = (now.toJSON()).slice(0,10);
+// click comfirm and show edit-area
+var comfirm = document.getElementById('comfirm');
+
+
+
+
+
+// click comfirm after , add the content to middle
+comfirm.onclick = function () {
   var tmp = '';
   tmp = '<ul>'
-        + '<li>' + time
+        + '<li>'
+          + 'time' + date.value + '</time>'
+          + '<div class="content">'
+          + '<span class="checkbox"></span>'
+          + '<input type="text" class="write">'
+          + '</div>'
+        + '</li>'
+        +'</ul>';
+  middle.appendChild(tmp);
+      
 }
+
+
+
+
+
+
+
+
