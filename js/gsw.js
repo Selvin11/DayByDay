e9283828eea4464985d460c5555e7dcd72d1018a
Selvin11 <528370630@qq.com>
2016-06-29 22:57:45 +0800

    all

--- js/gsw.js
+++ js/gsw.js
@@ -43,8 +43,10 @@ for (var i = 0; i < checkbox.length; i++) {
  */
 // click add btn and show the edit area
 var add = document.getElementById('add');
+var textarea = document.getElementById('text');
 var editArea = document.getElementsByClassName('edit-area')[0];
 add.onclick = function () {
+  // lock textarea mouseteer
   if (editArea.style.display === 'none') {
     editArea.style.display = 'block';
     add.style.transform = 'rotate3d(0, 0, 1, -45deg)';
@@ -67,25 +69,88 @@ date.value = (now.toJSON()).slice(0,10);
 var comfirm = document.getElementById('comfirm');
 
 
-
-
-
 // click comfirm after , add the content to middle
 comfirm.onclick = function () {
   var tmp = '';
-  tmp = '<ul>'
-        + '<li>'
-          + 'time' + date.value + '</time>'
+  tmp = '<li>'
+          + '<time>' + date.value + '</time>'
           + '<div class="content">'
           + '<span class="checkbox"></span>'
-          + '<input type="text" class="write">'
+          + '<input type="text" class="write" value="' + textarea.value + '">'
           + '</div>'
-        + '</li>'
-        +'</ul>';
-  middle.appendChild(tmp);
-      
-}
+        + '</li>';
+  // create a parent container and push it to middle container
+  var ul = document.createElement('ul');
+  ul.innerHTML = tmp;
+  middle.appendChild(ul);
+  //clear textarea messeage
+  textarea.value = '';
 
+  // again for in checkbox
+    for (var i = 0; i < checkbox.length; i++) {
+    checkbox[i].onclick = function () {
+      if(this.className == "checkbox"){
+        this.className = "checkbox done";
+        this.nextSibling.style.textDecoration = 'line-through';
+      }else{
+        this.className = "checkbox";
+        this.nextSibling.style.textDecoration = 'none';
+      }
+    }
+  }
+  // checkbox for end
+
+
+      // for list about all / complete / uncomplete / about
+      var all = document.getElementById('all'),
+          complete = document.getElementById('complete'),
+          uncomplete = document.getElementById('uncomplete'),
+          about = document.getElementById('about');
+      all.onclick = function () {
+        for (var i = 0; i < middle.children.length; i++) {
+          middle.children[i].style.display = 'block';
+        }
+      };
+      complete.onclick = function () {
+        for (var i = 0; i < checkbox.length; i++) {
+          if (checkbox[i].className == "checkbox done") {
+            checkbox[i].parentElement.parentElement.parentElement.style.display = 'block';
+          }else{
+            checkbox[i].parentElement.parentElement.parentElement.style.display = 'none';
+          }
+        }
+        
+      };
+      uncomplete.onclick = function () {
+        for (var i = 0; i < checkbox.length; i++) {
+          if (checkbox[i].className == "checkbox done") {
+            checkbox[i].parentElement.parentElement.parentElement.style.display = 'none';
+          }else{
+            checkbox[i].parentElement.parentElement.parentElement.style.display = 'blcok';
+          }
+        }
+      }
+
+      about.onclick = function () {
+        for (var i = 0; i < checkbox.length; i++) {
+          checkbox[i].parentElement.parentElement.parentElement.style.display = 'none';
+        }
+        var aboutCon = document.getElementById('about-content');
+        if(aboutCon.style.display == "none"){
+          aboutCon.style.display = 'block';
+        }else{
+          aboutCon.style.display = 'none';
+        }
+      }
+}
+about.onclick = function () {
+        var aboutCon = document.getElementById('about-content');
+        if(aboutCon.style.display == "none"){
+          aboutCon.style.display = 'block';
+        }else{
+          aboutCon.style.display = 'none';
+        }
+      }
 
 
 
